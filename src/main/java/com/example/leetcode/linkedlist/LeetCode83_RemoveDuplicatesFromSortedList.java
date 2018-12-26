311870d0c021dd0a3490c07fea21fbe55e98d73a
icecrea <798167096@qq.com>
2018-12-26 11:06:29 +0800

    'leetcode83'

--- src/main/java/com/example/leetcode/linkedlist/LeetCode83_RemoveDuplicatesFromSortedList.java
+++ src/main/java/com/example/leetcode/linkedlist/LeetCode83_RemoveDuplicatesFromSortedList.java
@@ -5,13 +5,13 @@ import com.example.leetcode.linkedlist.pojo.ListNode;
 /**
  * @description: 删除链表重复元素
  * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: 1->1->2
  * 输出: 1->2
  * 示例 2:
- *
+ * <p>
  * 输入: 1->1->2->3->3
  * 输出: 1->2->3
  * @author: icecrea
@@ -20,11 +20,12 @@ import com.example.leetcode.linkedlist.pojo.ListNode;
 public class LeetCode83_RemoveDuplicatesFromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
-        while (cur!=null && cur.next!=null){
-            while(cur.val ==cur.next.val){
-                cur.next=cur.next.next;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
             }
-            cur= cur.next;
         }
         return head;
     }
