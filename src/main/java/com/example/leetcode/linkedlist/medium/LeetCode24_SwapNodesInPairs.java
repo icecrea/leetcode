58eca4343b5445d2db20933186f08546a6539834
icecrea <798167096@qq.com>
2019-01-09 14:24:14 +0800

    '两两交换链表节点24'

---
+++ src/main/java/com/example/leetcode/linkedlist/medium/LeetCode24_SwapNodesInPairs.java
@@ -0,0 +1,37 @@
+package com.example.leetcode.linkedlist.medium;
+
+import com.example.leetcode.linkedlist.pojo.ListNode;
+import org.junit.Test;
+
+/**
+ * @description: 两两交换链表中的节点
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * 说明:
+ * <p>
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * @author: icecrea
+ * @create: 2019-01-08 22:06
+ **/
+public class LeetCode24_SwapNodesInPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+
+}
---
+++ src/main/java/com/example/leetcode/linkedlist/pojo/TreeNode.java
@@ -0,0 +1,16 @@
+package com.example.leetcode.linkedlist.pojo;
+
+/**
+ * @description:
+ * @author: icecrea
+ * @create: 2019-01-08 20:29
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
