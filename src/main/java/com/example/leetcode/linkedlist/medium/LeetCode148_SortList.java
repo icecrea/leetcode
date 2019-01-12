3a49b66441a9b8fc5b950f5e1f8d83b61f8c05cd
icecrea <798167096@qq.com>
2019-01-12 22:08:40 +0800

    'fix'

--- src/main/java/com/example/leetcode/linkedlist/medium/LeetCode148_SortList.java
+++ src/main/java/com/example/leetcode/linkedlist/medium/LeetCode148_SortList.java
@@ -18,5 +18,46 @@ import com.example.leetcode.linkedlist.pojo.ListNode;
  * @create: 2019-01-11 10:36
  **/
 public class LeetCode148_SortList {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode midNext = mid.next;
+        mid.next = null;
+        ListNode listNodeLeft = sortList(head);
+        ListNode listNodeRight = sortList(midNext);
+        return mergeSort(listNodeLeft, listNodeRight);
+    }
 
+    private ListNode getMid(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head, quick = head;
+        while (quick.next != null && quick.next.next != null) {
+            slow = slow.next;
+            quick = quick.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode mergeSort(ListNode n1, ListNode n2) {
+        ListNode preHead = new ListNode(0);
+        ListNode cur1 = n1;
+        ListNode cur2 = n2;
+        ListNode cur = preHead;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val < cur2.val) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = cur1 == null ? cur2 : cur1;
+        return preHead.next;
+    }
 }
