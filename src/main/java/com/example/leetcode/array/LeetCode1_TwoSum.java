216482d8f0bd36c439b293fa4ef8c7c794cca161
icecrea <798167096@qq.com>
2019-01-14 23:29:17 +0800

    'leetcode1_twosum'

---
+++ src/main/java/com/example/leetcode/array/LeetCode1_TwoSum.java
@@ -0,0 +1,60 @@
+package com.example.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * @author: icecrea
+ * @create: 2019-01-14 23:05
+ **/
+public class LeetCode1_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int size = (int) ((float) nums.length / 0.75F + 1.0F);
+        Map<Integer, Integer> map = new HashMap<>(size);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSum3(int[] nums, int target) {
+        int size = (int) ((float) nums.length / 0.75F + 1.0F);
+        Map<Integer, Integer> map = new HashMap<>(size);
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
