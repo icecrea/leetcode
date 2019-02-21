dd8f9311cb7466cab1fb396b5eed388d4d424921
icecrea <798167096@qq.com>
2019-02-21 23:47:10 +0800

    'arr26'

---
+++ src/main/java/com/example/topinterview/easy/array26.java
@@ -0,0 +1,91 @@
+package com.example.topinterview.easy;
+
+import java.util.*;
+
+/**
+ * @description: 两个数组的交集2
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * 示例 2:
+ * <p>
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ * 说明：
+ * <p>
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+ * 我们可以不考虑输出结果的顺序。
+ * 进阶:
+ * <p>
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ * @author: icecrea
+ * @create: 2019-02-20 10:49
+ **/
+public class array26 {
+    /**
+     * 用Map来建立nums1中字符和其出现个数之间的映射, 然后遍历nums2数组，
+     * 如果当前字符在Map中的个数大于0，则将此字符加入结果res中，然后Map的对应值自减1
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersect(int[] nums1, int[] nums2) {
+        List<Integer> tmp = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.get(nums1[i]) == null) {
+                map.put(nums1[i], 1);
+            } else {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            }
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) != 0) {
+                tmp.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+        int[] result = new int[tmp.size()];
+        int i = 0;
+        for (Integer e : tmp) {
+            result[i++] = e;
+        }
+        return result;
+    }
+
+    public int[] intersect2(int[] nums1, int[] nums2) {
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> tmp = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums2[j] > nums1[i]) {
+                i++;
+            } else if (nums2[j] < nums1[i]) {
+                j++;
+            } else {
+                tmp.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        int[] result = new int[tmp.size()];
+        for (int k = 0; k < result.length; k++) {
+            result[k] = tmp.get(k);
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/com/example/topinterview/easy/array27.java
@@ -0,0 +1,31 @@
+package com.example.topinterview.easy;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 加一
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * <p>
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * <p>
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 示例 2:
+ * <p>
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ * @author: icecrea
+ * @create: 2019-02-21 22:19
+ **/
+public class array27 {
+
+}
