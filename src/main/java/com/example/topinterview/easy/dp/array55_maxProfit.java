9f5f47236514a5605cb2288bdc387b5b7e48782a
icecrea <798167096@qq.com>
2019-03-18 19:35:01 +0800

    '买股票的最佳实际'

---
+++ src/main/java/com/example/topinterview/easy/dp/array55_maxProfit.java
@@ -0,0 +1,60 @@
+package com.example.topinterview.easy.dp;
+
+/**
+ * @description: 买股票的最佳时机
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ *
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ *
+ * 注意你不能在买入股票前卖出股票。
+ *
+ * 示例 1:
+ *
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 示例 2:
+ *
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * @author: icecrea
+ * @create: 2019-03-18 15:59
+ **/
+public class array55_maxProfit {
+    /**
+     * 最大利润用变量存起来
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+
+    /**
+     *
+     * maxCur = current maximum value  （以当前数卖出的最大利润）
+     *
+     * maxSoFar = maximum value found so far （到当前数为止能出现的最大利润）
+     * @param prices
+     * @return
+     */
+    public int maxProfit2(int[] prices) {
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
