90a70cce955f1acfe0d1e331c6ad835056b39e3b
icecrea <798167096@qq.com>
2019-03-18 15:59:07 +0800

    '爬楼梯'

---
+++ src/main/java/com/example/topinterview/easy/dp/array54_climbStairs.java
@@ -0,0 +1,50 @@
+package com.example.topinterview.easy.dp;
+
+/**
+ * @description: 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * <p>
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * <p>
+ * 注意：给定 n 是一个正整数。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ * <p>
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * 1 1
+ * 2 2
+ * 3 3
+ * 4 5
+ * 1111，112，121，211，22
+ * @author: icecrea
+ * @create: 2019-03-18 15:45
+ **/
+public class array54_climbStairs {
+
+    /**
+     * 递归时间复杂度不通过
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        int a[] = new int[n+1];
+        a[0] = 1;
+        a[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            a[i] = a[i - 1] + a[i - 2];
+        }
+        return a[n];
+    }
+}
