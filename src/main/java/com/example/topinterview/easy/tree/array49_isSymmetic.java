17d5f2364662a40a27622b9c9a8830bbd54ad699
icecrea <798167096@qq.com>
2019-03-18 12:03:49 +0800

    '二叉树层次遍历'

---
+++ src/main/java/com/example/topinterview/easy/tree/array49_isSymmetic.java
@@ -0,0 +1,50 @@
+package com.example.topinterview.easy.tree;
+
+import com.example.leetcode.linkedlist.pojo.TreeNode;
+
+/**
+ * @description: 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * 说明:
+ *
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ * @author: icecrea
+ * @create: 2019-03-18 11:48
+ **/
+public class array49_isSymmetic {
+    public boolean isSymmetrical(TreeNode pRoot) {
+        if (pRoot == null) {
+            return true;
+        }
+        return helper(pRoot.left, pRoot.right);
+    }
+
+    private boolean helper(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return helper(root1.left, root2.right) && helper(root1.right, root2.left);
+
+    }
+}
