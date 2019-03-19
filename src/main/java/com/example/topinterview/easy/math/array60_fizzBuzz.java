f2c5cccbbead6d3c97113ea4d2f24b9ca9e78b72
icecrea <798167096@qq.com>
2019-03-19 12:06:08 +0800

    '质数统计和输出倍数'

---
+++ src/main/java/com/example/topinterview/easy/math/array60_fizzBuzz.java
@@ -0,0 +1,91 @@
+package com.example.topinterview.easy.math;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description:Fizz Buzz
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ * <p>
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ * <p>
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ * <p>
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ * <p>
+ * 示例：
+ * <p>
+ * n = 15,
+ * <p>
+ * 返回:
+ * [
+ * "1",
+ * "2",
+ * "Fizz",
+ * "4",
+ * "Buzz",
+ * "Fizz",
+ * "7",
+ * "8",
+ * "Fizz",
+ * "Buzz",
+ * "11",
+ * "Fizz",
+ * "13",
+ * "14",
+ * "FizzBuzz"
+ * ]
+ * @author: icecrea
+ * @create: 2019-03-19 10:29
+ **/
+public class array60_fizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 可以通过计数++ 再清零 循环操作 避免%
+     * @param n
+     * @return
+     */
+    public List<String> fizzBuzz2(int n) {
+        List<String> ret = new ArrayList<String>(n);
+        for(int i=1,fizz=0,buzz=0;i<=n ;i++){
+            fizz++;
+            buzz++;
+            if(fizz==3 && buzz==5){
+                ret.add("FizzBuzz");
+                fizz=0;
+                buzz=0;
+            }else if(fizz==3){
+                ret.add("Fizz");
+                fizz=0;
+            }else if(buzz==5){
+                ret.add("Buzz");
+                buzz=0;
+            }else{
+                ret.add(String.valueOf(i));
+            }
+        }
+        return ret;
+    }
+
+    @Test
+    public void test() {
+        fizzBuzz(3);
+    }
+}
---
+++ src/main/java/com/example/topinterview/easy/math/array61_countPrimes.java
@@ -0,0 +1,30 @@
+package com.example.topinterview.easy.math;
+
+/**
+ * @description: 计数质数
+ * 统计所有小于非负整数 n 的质数的数量。
+ *
+ * 示例:
+ *
+ * 输入: 10
+ * 输出: 4
+ * 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+ * @author: icecrea
+ * @create: 2019-03-19 10:42
+ **/
+public class array61_countPrimes {
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    notPrime[i * j] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+}
