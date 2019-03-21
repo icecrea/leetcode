2e37a2f7dd37f6245253a6d328871bdddcc37281
icecrea <798167096@qq.com>
2019-03-21 22:50:50 +0800

    '正确闭合'

---
+++ src/main/java/com/example/topinterview/easy/other/array68_isValid.java
@@ -0,0 +1,37 @@
+package com.example.topinterview.easy.other;
+
+import java.util.Stack;
+
+/**
+ * @description:有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "
+ * @author: icecrea
+ * @create: 2019-03-21 22:40
+ **/
+public class array68_isValid {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
