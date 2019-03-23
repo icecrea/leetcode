bbe2d4612ae61e42e73b762cbd1fbb1deb3610dc
icecrea <798167096@qq.com>
2019-03-23 17:16:35 +0800

    '帕斯卡三角和缺失的数字'

---
+++ src/main/java/com/example/topinterview/easy/other/array67_pascalTriangle.java
@@ -0,0 +1,50 @@
+package com.example.topinterview.easy.other;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 帕斯卡三角形
+ * 定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * <p>
+ * <p>
+ * <p>
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 5
+ * 输出:
+ * [
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
+ * [1,4,6,4,1]
+ * ]
+ * @author: icecrea
+ * @create: 2019-03-22 15:51
+ **/
+public class array67_pascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+            allrows.add(new ArrayList<Integer>(row));
+        }
+        return allrows;
+
+    }
+
+    @Test
+    public void test(){
+        generate(4);
+    }
+
+}
---
+++ src/main/java/com/example/topinterview/easy/other/array69_missingNumber.java
@@ -0,0 +1,32 @@
+package com.example.topinterview.easy.other;
+
+/**
+ * @description:缺失数字 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,0,1]
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ * 说明:
+ * 你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+ * @author: icecrea
+ * @create: 2019-03-22 15:53
+ **/
+public class array69_missingNumber {
+    /**
+     * 1到n的和 - 数组和
+     * @param nums
+     * @return
+     */
+    public int missingNumber(int[] nums) {
+        int sum = 0, n = nums.length;
+        for (int i : nums) {
+            sum += i;
+        }
+        return n * (n + 1) / 2 - sum;
+    }
+}
