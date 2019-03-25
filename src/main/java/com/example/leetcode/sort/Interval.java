03a4193a75db0f759ddac0ba819000b5cb3fd8b8
icecrea <798167096@qq.com>
2019-03-25 09:37:50 +0800

    '合并区间'

---
+++ src/main/java/com/example/leetcode/sort/Interval.java
@@ -0,0 +1,13 @@
+package com.example.leetcode.sort;
+
+/**
+ * @description:
+ * @author: icecrea
+ * @create: 2019-03-25 09:09
+ **/
+public class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
---
+++ src/main/java/com/example/leetcode/sort/LeetCode56_MergeIntervals.java
@@ -0,0 +1,75 @@
+package com.example.leetcode.sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description: 合并区间
+ * 给出一个区间的集合，请合并所有重叠的区间。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [[1,3],[2,6],[8,10],[15,18]]
+ * 输出: [[1,6],[8,10],[15,18]]
+ * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2:
+ * <p>
+ * 输入: [[1,4],[4,5]]
+ * 输出: [[1,5]]
+ * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * @author: icecrea
+ * @create: 2019-03-25 09:08
+ **/
+public class LeetCode56_MergeIntervals {
+
+    public List<Interval> merge(List<Interval> intervals) {
+        if (intervals.size() <= 1) {
+            return intervals;
+        }
+        intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+
+        List<Interval> result = new ArrayList<>();
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+
+        for (Interval interval : intervals) {
+            if (interval.start <= end) {
+                end = Math.max(end, interval.end);
+            } else {
+                result.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+
+        result.add(new Interval(start, end));
+        return result;
+    }
+
+    /**
+     * 更快的方法
+     *
+     * @param intervals
+     * @return
+     */
+    public List<Interval> merge2(List<Interval> intervals) {
+        int n = intervals.size();
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals.get(i).start;
+            ends[i] = intervals.get(i).end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        List<Interval> res = new ArrayList<>();
+        for (int i = 0, j = 0; i < n; i++) {
+            if (i == n - 1 || starts[i + 1] > ends[i]) {
+                res.add(new Interval(starts[j], ends[i]));
+                j = i + 1;
+            }
+        }
+        return res;
+    }
+}
