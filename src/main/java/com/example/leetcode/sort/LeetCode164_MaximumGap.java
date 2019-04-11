e1e2dc45e400d33ca67562d6a969c039e1f7ac09
icecrea <798167096@qq.com>
2019-04-11 21:31:34 +0800

    '最大间距-桶排序题目'

---
+++ src/main/java/com/example/leetcode/sort/LeetCode164_MaximumGap.java
@@ -0,0 +1,81 @@
+package com.example.leetcode.sort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @description: 最大间距
+ * 给定一个无序的数组，找出数组在排序之后，相邻元素之间最大的差值。
+ * <p>
+ * 如果数组元素个数小于 2，则返回 0。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,6,9,1]
+ * 输出: 3
+ * 解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+ * 示例 2:
+ * <p>
+ * 输入: [10]
+ * 输出: 0
+ * 解释: 数组元素个数小于 2，因此返回 0。
+ * 说明:
+ * <p>
+ * 你可以假设数组中所有元素都是非负整数，且数值在 32 位有符号整数范围内。
+ * 请尝试在线性时间复杂度和空间复杂度的条件下解决此问题。
+ * @author: icecrea
+ * @create: 2019-04-11 20:11
+ **/
+public class LeetCode164_MaximumGap {
+
+
+    public int maximumGap(int[] num) {
+        if (num == null || num.length < 2) {
+            return 0;
+        }
+        int min = num[0];
+        int max = num[0];
+        for (int i : num) {
+            min = Math.min(min, i);
+            max = Math.max(max, i);
+        }
+        // the minimum possibale gap, ceiling of the integer division
+        int gap = (int) Math.ceil((double) (max - min) / (num.length - 1));
+        int[] bucketsMIN = new int[num.length - 1];
+        int[] bucketsMAX = new int[num.length - 1];
+        Arrays.fill(bucketsMIN, Integer.MAX_VALUE);
+        Arrays.fill(bucketsMAX, Integer.MIN_VALUE);
+        // put numbers into buckets
+        for (int i : num) {
+            if (i == min || i == max) {
+                continue;
+            }
+            int idx = (i - min) / gap;
+            bucketsMIN[idx] = Math.min(i, bucketsMIN[idx]);
+            bucketsMAX[idx] = Math.max(i, bucketsMAX[idx]);
+        }
+        // scan the buckets for the max gap
+        int maxGap = Integer.MIN_VALUE;
+        int previous = min;
+        for (int i = 0; i < num.length - 1; i++) {
+            if (bucketsMIN[i] == Integer.MAX_VALUE && bucketsMAX[i] == Integer.MIN_VALUE) {
+                // empty bucket
+                continue;
+            }
+            // min value minus the previous value is the current gap
+            maxGap = Math.max(maxGap, bucketsMIN[i] - previous);
+            // update previous bucket value
+            previous = bucketsMAX[i];
+        }
+        // updata the final max value gap
+        maxGap = Math.max(maxGap, max - previous);
+        return maxGap;
+    }
+
+    @Test
+    public void test() {
+        int[] zz = new int[]{3, 6, 9, 1, 15};
+        System.out.println(maximumGap(zz));
+    }
+}
