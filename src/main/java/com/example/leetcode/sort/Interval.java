7564ca0b86ac1db2afb2ab1c7f62c51f247bfc7c
icecrea <798167096@qq.com>
2019-04-11 20:06:57 +0800

    '插入区间（变相版合并区间）'

--- src/main/java/com/example/leetcode/sort/Interval.java
+++ src/main/java/com/example/leetcode/sort/Interval.java
@@ -10,4 +10,13 @@ public class Interval {
     int end;
     Interval() { start = 0; end = 0; }
     Interval(int s, int e) { start = s; end = e; }
+
+    @Override
+    public String
+    toString() {
+        return "Interval{" +
+                "start=" + start +
+                ", end=" + end +
+                '}';
+    }
 }
---
+++ src/main/java/com/example/leetcode/sort/LeetCode57_InsertInterval.java
@@ -0,0 +1,84 @@
+package com.example.leetcode.sort;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description: 插入区间
+ * 给出一个无重叠的 ，按照区间起始端点排序的区间列表。
+ * <p>
+ * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * 输出: [[1,5],[6,9]]
+ * 示例 2:
+ * <p>
+ * 输入: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * 输出: [[1,2],[3,10],[12,16]]
+ * 解释: 这是因为新的区间 [4,8] 与 [3,5],[6,7],[8,10] 重叠。
+ * @author: icecrea
+ * @create: 2019-04-11 18:47
+ **/
+public class LeetCode57_InsertInterval {
+
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        intervals.add(newInterval);
+        int n = intervals.size();
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals.get(i).start;
+            ends[i] = intervals.get(i).end;
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        List<Interval> res = new ArrayList<>();
+        for (int i = 0, j = 0; i < n; i++) {
+            if (i == n - 1 || starts[i + 1] > ends[i]) {
+                res.add(new Interval(starts[j], ends[i]));
+                j = i + 1;
+            }
+        }
+        return res;
+    }
+
+    public List<Interval> insert2(List<Interval> intervals, Interval newInterval) {
+        intervals.add(newInterval);
+        intervals.sort((i1, i2) -> Integer.compare(i1.start, i2.start));
+
+        List<Interval> result = new ArrayList<>();
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+
+        for (Interval interval : intervals) {
+            if (interval.start <= end) {
+                end = Math.max(end, interval.end);
+            } else {
+                result.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
+            }
+        }
+
+        result.add(new Interval(start, end));
+        return result;
+    }
+
+
+    @Test
+    public void test(){
+        List<Interval> list = new ArrayList<>();
+        list.add(new Interval(1,2));
+        list.add(new Interval(3,5));
+        list.add(new Interval(6,7));
+        list.add(new Interval(8,10));
+        list.add(new Interval(12,16));
+        List<Interval> insert = insert(list, new Interval(4, 8));
+        System.out.println(insert);
+    }
+}
