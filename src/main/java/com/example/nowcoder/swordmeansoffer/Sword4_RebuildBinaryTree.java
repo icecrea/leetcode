634ab85ccc12af393f81368ed22c65ed754a2699
icecrea <798167096@qq.com>
2019-04-17 13:27:57 +0800

    '两个栈实现队列'

--- src/main/java/com/example/nowcoder/swordmeansoffer/Sword4_RebuildBinaryTree.java
+++ src/main/java/com/example/nowcoder/swordmeansoffer/Sword4_RebuildBinaryTree.java
@@ -36,7 +36,7 @@ public class Sword4_RebuildBinaryTree {
         for (int i = startIn; i <= endIn; i++) {
             if (in[i] == pre[startPre]) {
                 root.left = reConstructBinaryTree(pre, startPre + 1, startPre + i - startIn, in, startIn, i - 1);
-                root.right = reConstructBinaryTree(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+                root.right = reConstructBinaryTree(pre, startPre + i - startIn + 1, endPre, in, i + 1, endIn);
                 break;
             }
         }
