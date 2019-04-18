2e1cf85bea72ab49a380591543ceedaf48dac3c3
icecrea <798167096@qq.com>
2019-04-18 13:25:57 +0800

    '奇数位于偶数前'

--- src/main/java/com/example/nowcoder/swordmeansoffer/Sword13_ReOrderArray.java
+++ src/main/java/com/example/nowcoder/swordmeansoffer/Sword13_ReOrderArray.java
@@ -8,50 +8,14 @@ package com.example.nowcoder.swordmeansoffer;
  **/
 public class Sword13_ReOrderArray {
 
+
     /**
-     * 用例:
-     * [1,2,3,4,5,6,7]
-     * <p>
-     * 对应输出应该为:
-     * <p>
-     * [1,3,5,7,2,4,6]
-     * <p>
-     * 你的输出为:
-     * <p>
-     * [1,3,5,7,2,6,4]
-     * <p>
-     * 1354267
-     * 1356264
-     * <p>
-     * 1234567
-     * 1334567
-     * 1354567
+     * 找到第一个偶数 和其后的第一个奇数
+     * 中间数字整体后移一位， 奇数放到偶数位置
      *
      * @param a
      */
     public void reOrderArray(int[] a) {
-        int i = 0;
-        while (i <= a.length - 1) {
-            if ((a[i] & 1) == 0) {
-                for (int j = i + 1; j <= a.length - 1; j++) {
-                    if ((a[j] & 1) == 1) {
-                        swap(a, i, j);
-                        break;
-                    }
-                }
-            }
-            i++;
-        }
-    }
-
-    private void swap(int[] a, int i, int j) {
-        int t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-    }
-
-
-    public void reOrderArray2(int[] a) {
         if (a == null || a.length == 0) {
             return;
         }
