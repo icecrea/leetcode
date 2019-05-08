987b7c8c1929909cb94bccc45eb868dba1baa3ff
icecrea <798167096@qq.com>
2019-05-08 18:05:25 +0800

    '丑数'

---
+++ src/main/java/com/example/nowcoder/swordmeansoffer/Sword33_FirstNotRepeatChar.java
@@ -0,0 +1,14 @@
+package com.example.nowcoder.swordmeansoffer;
+
+/**
+ * @description: 第一个只出现一次的字符
+ * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置,
+ * 如果没有则返回 -1（需要区分大小写）.
+ * @author: icecrea
+ * @create: 2019-05-08 17:57
+ **/
+public class Sword33_FirstNotRepeatChar {
+    public int FirstNotRepeatingChar(String str) {
+        return 0;
+    }
+}
