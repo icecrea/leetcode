19d3fab128d4f39dda47992a56b7330f98fac20a
icecrea <798167096@qq.com>
2019-09-11 16:35:32 +0800

    '对称的二叉树'

--- src/main/java/com/example/swordoffer/Sword55_GetNext.java
+++ src/main/java/com/example/swordoffer/Sword55_GetNext.java
@@ -2,18 +2,18 @@ package com.example.swordoffer;
 
 /**
  * @description: 二叉树下一个节点
- *
+ * <p>
  * 题目描述
  * 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
- *
  * @author: icecrea
  * @create: 2019-09-11 12:59
  **/
 public class Sword55_GetNext {
 
-    public TreeLinkNode GetNext(TreeLinkNode pNode)
-    {
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if (pNode.right != null) {
 
+        }
     }
 
     public class TreeLinkNode {
