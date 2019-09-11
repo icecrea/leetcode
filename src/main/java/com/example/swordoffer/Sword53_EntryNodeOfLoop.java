959a2726a57a09f63e29d31140a93652f6aa2d3a
icecrea <798167096@qq.com>
2019-09-11 14:55:17 +0800

    '链表中环入口节点'

--- src/main/java/com/example/swordoffer/Sword53_EntryNodeOfLoop.java
+++ src/main/java/com/example/swordoffer/Sword53_EntryNodeOfLoop.java
@@ -1,5 +1,6 @@
 package com.example.swordoffer;
 
+import com.example.leetcode.linkedlist.medium.LeetCode142_LinkedListCycle2;
 import com.example.leetcode.linkedlist.pojo.ListNode;
 
 /**
@@ -9,8 +10,38 @@ import com.example.leetcode.linkedlist.pojo.ListNode;
  * @create: 2019-09-11 12:58
  **/
 public class Sword53_EntryNodeOfLoop {
-    public ListNode EntryNodeOfLoop(ListNode pHead)
-    {
 
+    /**
+     * 思路参考 LEETCODE142题
+     * @see LeetCode142_LinkedListCycle2
+     * @param pHead
+     * @return
+     */
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        if (pHead == null) {
+            return null;
+        }
+        ListNode fast = pHead;
+        ListNode slow = pHead;
+
+        boolean isCycle = false;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                isCycle = true;
+                break;
+            }
+        }
+
+        if (isCycle) {
+            ListNode p = pHead;
+            while (p != slow) {
+                p = p.next;
+                slow = slow.next;
+            }
+        }
+
+        return slow;
     }
 }
