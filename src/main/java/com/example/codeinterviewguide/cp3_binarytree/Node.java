daf8e63448822a4cae63965b4e9e25848ce12a9f
icecrea <798167096@qq.com>
2019-10-16 23:35:56 +0800

    lru-二叉树

---
+++ src/main/java/com/example/codeinterviewguide/cp3_binarytree/Node.java
@@ -0,0 +1,17 @@
+package com.example.codeinterviewguide.cp3_binarytree;
+
+/**
+ * @description:
+ * @author: icecrea
+ * @create: 2019-10-02
+ **/
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+
+}
---
+++ src/main/java/com/example/codeinterviewguide/cp3_binarytree/p01_PreInPosTraversal.java
@@ -0,0 +1,121 @@
+package com.example.codeinterviewguide.cp3_binarytree;
+
+import java.util.Stack;
+
+/**
+ * @description: 二叉树前序中序后序遍历(使用递归和非递归方式)
+ * @author: icecrea
+ * @create: 2019-10-02
+ **/
+public class p01_PreInPosTraversal {
+
+    public static void preOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.value + " ");
+        preOrderRecur(head.left);
+        preOrderRecur(head.right);
+    }
+
+    public static void inOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        inOrderRecur(head.left);
+        System.out.print(head.value + " ");
+        inOrderRecur(head.right);
+    }
+
+    public static void posOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        posOrderRecur(head.left);
+        posOrderRecur(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    public static void preOrderUnRecur(Node head) {
+        System.out.print("pre-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.add(head);
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                System.out.print(head.value + " ");
+                if (head.right != null) {
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void inOrderUnRecur(Node head) {
+        System.out.print("in-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
+                    stack.push(head);
+                    head = head.left;
+                } else {
+                    head = stack.pop();
+                    System.out.print(head.value + " ");
+                    head = head.right;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void posOrderUnRecur1(Node head) {
+        System.out.print("pos-order: ");
+        if (head != null) {
+            Stack<Node> s1 = new Stack<>();
+            Stack<Node> s2 = new Stack<>();
+            s1.push(head);
+            while (!s1.isEmpty()) {
+                head = s1.pop();
+                s2.push(head);
+                if (head.left != null) {
+                    s1.push(head.left);
+                }
+                if (head.right != null) {
+                    s1.push(head.right);
+                }
+            }
+            while (!s2.isEmpty()) {
+                System.out.print(s2.pop().value + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void posOrderUnRecur2(Node h) {
+        System.out.print("pos-order: ");
+        if (h != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.push(h);
+            Node c;
+            while (!stack.isEmpty()) {
+                c = stack.peek();
+                if (c.left != null && h != c.left && h != c.right) {
+                    stack.push(c.left);
+                } else if (c.right != null && h != c.right) {
+                    stack.push(c.right);
+                } else {
+                    System.out.print(stack.pop().value + " ");
+                    h = c;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+
+}
