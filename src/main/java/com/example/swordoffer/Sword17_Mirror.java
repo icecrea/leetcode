c72523bb7ec8dd09afdb02c313cccb99355ab850
icecrea <798167096@qq.com>
2019-10-23 19:33:19 +0800

    '二叉树镜像-非递归方法'

--- src/main/java/com/example/swordoffer/Sword17_Mirror.java
+++ src/main/java/com/example/swordoffer/Sword17_Mirror.java
@@ -2,22 +2,24 @@ package com.example.swordoffer;
 
 import com.example.leetcode.linkedlist.pojo.TreeNode;
 
+import java.util.Stack;
+
 /**
  * @description: 二叉树的镜像
  * 操作给定的二叉树，将其变换为源二叉树的镜像。
  * 输入描述:
  * 二叉树的镜像定义：源二叉树
- *     	    8
- *     	   /  \
- *     	  6   10
- *     	 / \  / \
- *     	5  7 9 11
- *     	镜像二叉树
- *     	    8
- *     	   /  \
- *     	  10   6
- *     	 / \  / \
- *     	11 9 7  5
+ * 8
+ * /  \
+ * 6   10
+ * / \  / \
+ * 5  7 9 11
+ * 镜像二叉树
+ * 8
+ * /  \
+ * 10   6
+ * / \  / \
+ * 11 9 7  5
  * @author: icecrea
  * @create: 2019-04-18 19:42
  **/
@@ -25,12 +27,14 @@ public class Sword17_Mirror {
 
     /**
      * 从上到下,先交换根节点的左右节点，然后对根节点的左右节点递归进行交换
+     *
      * @param root
      */
-    public void Mirror(TreeNode root) {
+    public void MirrorRecur(TreeNode root) {
         if (root == null) {
             return;
         }
+        //左子树和右子树都为空，不需要交换
         if (root.left == null && root.right == null) {
             return;
         }
@@ -44,6 +48,35 @@ public class Sword17_Mirror {
         }
         if (root.right != null) {
             Mirror(root.right);
+        }
+    }
+
+
+    /**
+     * 非递归解法
+     * @param root
+     */
+    public void Mirror(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack();
+        stack.push(root);
+
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            //左子树不为空或者右子树不为空，都需要交换
+            if (node.left != null || node.right != null) {
+                TreeNode tmp = node.left;
+                node.left = node.right;
+                node.right = tmp;
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
         }
     }
 
