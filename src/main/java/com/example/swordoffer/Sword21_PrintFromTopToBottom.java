48eeb029f53f8fec4977b00c9a6142bd1d7dccc6
icecrea <798167096@qq.com>
2019-10-23 19:47:34 +0800

    '从上往下打印二叉树且每层打印出换行符'

--- src/main/java/com/example/swordoffer/Sword21_PrintFromTopToBottom.java
+++ src/main/java/com/example/swordoffer/Sword21_PrintFromTopToBottom.java
@@ -3,6 +3,8 @@ package com.example.swordoffer;
 import com.example.leetcode.linkedlist.pojo.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @description: 从上往下打印二叉树
@@ -30,6 +32,38 @@ public class Sword21_PrintFromTopToBottom {
             list.add(temp.val);
         }
         return list;
-
     }
+
+    /**
+     * 进阶版，从上往下打印二叉树，要求每层打印出一个换行符，即打印出每层的信息
+     *
+     * @param root
+     * @return
+     */
+    public ArrayList<ArrayList<Integer>> PrintFromTopToBottomWithLevel(TreeNode root) {
+        ArrayList<ArrayList<Integer>> listAll = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList();
+        if (root == null) {
+            return listAll;
+        }
+        q.offer(root);
+        while (!q.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            listAll.add(list);
+        }
+
+        return listAll;
+    }
+
 }
