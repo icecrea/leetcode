96fd520f89d7066ff92463bc62b06c4df0087d92
icecrea <798167096@qq.com>
2019-10-24 11:35:07 +0800

    '二叉搜索树转双向链表-递归方法'

--- src/main/java/com/example/swordoffer/Sword25_BSTToDoubleLinkedList.java
+++ src/main/java/com/example/swordoffer/Sword25_BSTToDoubleLinkedList.java
@@ -41,4 +41,47 @@ public class Sword25_BSTToDoubleLinkedList {
         }
         return root;
     }
+
+
+    private TreeNode head = null;
+    private TreeNode last = null;
+
+    public TreeNode Convert(TreeNode root) {
+        visit(root);
+        return head;
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param root
+     */
+    private void visit(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        visit(root.left);
+        createList(root);
+        visit(root.right);
+    }
+
+    /**
+     * 建表
+     *
+     * @param node
+     */
+    private void createList(TreeNode node) {
+        //当前结点左结点连接上一个
+        node.left = last;
+        //为空说明当前结点是第一个结点
+        if (last == null) {
+            head = node;
+        } else {
+            //不为空要将当前结点赋给上个结点的右结点
+            last.right = node;
+        }
+        //更新last
+        last = node;
+    }
+
 }
