829f2d663ee406ea8b00e8bbed80504afcc303e8
icecrea <798167096@qq.com>
2019-11-06 23:37:59 +0800

    '滑动窗口最大值'

--- src/main/java/com/example/swordoffer/Sword33_FirstNotRepeatChar.java
+++ src/main/java/com/example/swordoffer/Sword33_FirstNotRepeatChar.java
@@ -1,9 +1,9 @@
 package com.example.swordoffer;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 
 /**
- * @description: 第一个只出现一次的字符 TODO
+ * @description: 第一个只出现一次的字符的位置
  * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置,
  * 如果没有则返回 -1（需要区分大小写）.
  * @author: icecrea
@@ -11,7 +11,7 @@ import java.util.LinkedHashMap;
  **/
 public class Sword33_FirstNotRepeatChar {
     public int FirstNotRepeatingChar(String str) {
-        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < str.length(); i++) {
             if (map.containsKey(str.charAt(i))) {
                 int time = map.get(str.charAt(i));
--- src/main/java/com/example/swordoffer/Sword62_MaxInWindow.java
+++ src/main/java/com/example/swordoffer/Sword62_MaxInWindow.java
@@ -1,6 +1,7 @@
 package com.example.swordoffer;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * 滑动窗口最大值：
@@ -10,18 +11,35 @@ import java.util.*;
  */
 public class Sword62_MaxInWindow {
 
-    public ArrayList<Integer> maxInWindows(int[] num, int size) {
-        Queue q = new LinkedList<Integer>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(size, Collections.reverseOrder());
-        for (int i = 0; i < size; i++) {
-            q.offer(num[i]);
-            maxHeap.offer(num[i]);
+    /**
+     * 思路：滑动窗口应当是队列，但为了得到滑动窗口的最大值，队列序可以从两端删除元素，因此使用双端队列。队列第一个位置保存当前窗口的最大值。
+     * 原则：对新来的元素k，将其与双端队列中的元素相比较
+     * 1）前面比k小的，直接移出队列（因为不再可能成为后面滑动窗口的最大值了!）,
+     * 2）前面比k大的X，比较两者下标，判断X是否已不在窗口之内，不在了，直接移出队列
+     */
+    public ArrayList<Integer> maxInWindows1(int[] num, int size) {
+        if (num == null || num.length == 0 || size <= 0 || num.length < size) {
+            return new ArrayList<>();
         }
-        //
-        List result = new ArrayList();
-        for (int i = size; i < num.length; i++) {
-            result.add(maxHeap.poll());
-            q.offer(num[i]);
+        ArrayList<Integer> result = new ArrayList<>();
+        //双端队列，用来记录每个窗口的最大值下标
+        LinkedList<Integer> qmax = new LinkedList<>();
+        for (int i = 0; i < num.length; i++) {
+            //队列左边元素最大
+            while (!qmax.isEmpty() && num[qmax.peekLast()] < num[i]) {
+                qmax.pollLast();
+            }
+            qmax.addLast(i);
+            //判断队首元素是否已经在窗口之外
+            if (qmax.peekFirst() == i - size) {
+                qmax.pollFirst();
+            }
+            //向result列表中加入元素
+            if (i >= size - 1) {
+                result.add(num[qmax.peekFirst()]);
+            }
         }
+        return result;
     }
+
 }
