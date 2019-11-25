71b84e9cff39f737a74846494a9cf23f4759372d
icecrea <798167096@qq.com>
2019-11-25 22:40:11 +0800

    买卖股票最佳时机

---
+++ src/main/java/com/example/leetcode/dp/LeetCode121_BestTimeToBuyAndSellStock.java
@@ -0,0 +1,62 @@
+package com.example.leetcode.dp;
+
+/**
+ * 买卖股票的最佳时机
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ *
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ *
+ * 注意你不能在买入股票前卖出股票。
+ *
+ * 示例 1:
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ *
+ * 示例 2:
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ */
+public class LeetCode121_BestTimeToBuyAndSellStock {
+
+    /**
+     * 可以将数组在xy坐标轴上画出，可以发现本题需要找到最小的谷和之后的最大的峰
+     * 定义两个变量 minprice，maxprofit
+     *
+     * arr       7,1,5,3,6,4    7 2 6 1 3
+     * minprice  7 1 1 1 1 1    7 2 2 1 1
+     * maxprofit 0 0 4 4 5 5    0 0 4 4 4
+     */
+    public int maxProfit(int[] prices) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+
+
+    /**
+     * 暴力法 O(n^2)
+     */
+    public int maxProfit2(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit) {
+                    maxprofit = profit;
+                }
+            }
+        }
+        return maxprofit;
+    }
+
+}
