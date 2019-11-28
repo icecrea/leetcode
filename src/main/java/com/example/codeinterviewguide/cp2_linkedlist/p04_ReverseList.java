fa62bdcb83dc63e1f1d1db437e39b3c47a348fc6
icecrea <798167096@qq.com>
2019-11-28 16:02:43 +0800

    反转链表修正

--- src/main/java/com/example/codeinterviewguide/cp2_linkedlist/p04_ReverseList.java
+++ src/main/java/com/example/codeinterviewguide/cp2_linkedlist/p04_ReverseList.java
@@ -11,11 +11,11 @@ public class p04_ReverseList {
 
     /**
      * 反转链表简洁版
+     *
      * @param head
-     * @param next1
      * @return
      */
-    public static ListNode reverseList(ListNode head, ListNode next1) {
+    public static ListNode reverseList(ListNode head) {
         ListNode pre = null;
         ListNode next = null;
         while (head != null) {
@@ -27,5 +27,22 @@ public class p04_ReverseList {
         return pre;
     }
 
+    /**
+     * reverse(head) 定义：
+     * 输入一个节点 head，将「以 head 为起点」的链表反转，并返回反转之后的头结点。
+     *
+     */
+    public static ListNode reverse(ListNode head) {
+        //递归终止条件： 找到链表最后一个节点
+        if (head.next == null) {
+            return head;
+        }
+        ListNode last = reverse(head.next);
+        //反转头节点的下一个节点的next指针 head.next对应的是 反转后链表的尾指针，而node.next是反转后的头指针的下一个节点，此处注意区分
+        head.next.next = head;
+        //反转头节点的next指针，将头节点作为尾节点
+        head.next = null;
+        return last;
+    }
 
 }
