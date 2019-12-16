293aeffa11dffabd7007d40a38c55b0d64929111
icecrea <798167096@qq.com>
2019-12-16 13:49:29 +0800

    '最长回文子串-动态规划更新'

--- src/main/java/com/example/leetcode/dp/LeetCode05_LongestPalindromicSubstring.java
+++ src/main/java/com/example/leetcode/dp/LeetCode05_LongestPalindromicSubstring.java
@@ -5,14 +5,14 @@ import org.junit.Test;
 /**
  * @description: 最长回文子串
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入: "babad"
  * 输出: "bab"
  * 注意: "aba" 也是一个有效答案。
  * 示例 2：
- *
+ * <p>
  * 输入: "cbbd"
  * 输出: "bb"
  * @author: icecrea
@@ -51,47 +51,34 @@ public class LeetCode05_LongestPalindromicSubstring {
     }
 
     /**
-     * TODO dp方法
+     * 动态规划方法
      * dp[i][j] str[i] - str[j]是否是回文子串
      * dp[i][j] = dp[i+1][j-1] && str[i]==str[j]
      * 初始化：对角线为单个字符，设为0
      * 初始化一字母和二字母的回文，然后找到所有三字母回文，并依此类推
      */
     public String longestPalindrome2(String s) {
-        //  长度为1，返回当前串
-        if (s.length() == 1) {
+        if (s.length() == 0) {
             return s;
         }
-        //长度为2并且两个字符相等则返回
-        if (s.length() == 2 && s.charAt(0) == s.charAt(1)) {
-            return s;
-        }
-        //用于标记dp[j][i]即从j到i是否是回文串；
-        //如dp[1][5]＝＝true则表示字符串索引位置从1到5的子串是回文串。
-        boolean[][] dp = new boolean[s.length()][s.length()];
-        //最长回文串初始最大为0
-        int maxlen = 0;
-        //对应的maxlen的开始索引位置
-        int beginIndex = 0;
-        //对应的maxlen的结束索引位置
-        int lastIndex = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int j = i;
-            while (j >= 0) {
-                //满足上述的第三个条件，即当前s.charAt(i)==s.charAt(j)并
-                //且s[j＋1到i－1]也是回文串
-                if (s.charAt(i) == s.charAt(j) && (i - j < 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (maxlen < i - j + 1) {
-                        beginIndex = j;
-                        lastIndex = i + 1;
-                        maxlen = i - j + 1;
+        //用于标记dp[i][j]即从j到i是否是回文串，如dp[1][5]＝＝true则表示字符串索引位置从1到5的子串是回文串。
+        boolean dp[][] = new boolean[s.length()][s.length()];
+        int max = 0, start = 0, end = 0;
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = j; i >= 0; i--) {
+                //满足上述的第三个条件，即当前s.charAt(i)==s.charAt(j)并且s[j＋1到i－1]也是回文串
+                //i-j<2 是考虑单个字符+两个字符的回文串情况
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    if (j - i + 1 > max) {
+                        max = j - i + 1;
+                        start = i;
+                        end = j;
                     }
                 }
-                j--;
             }
         }
-        return s.substring(beginIndex, lastIndex);
+        return s.substring(start, end + 1);
     }
 
     @Test
