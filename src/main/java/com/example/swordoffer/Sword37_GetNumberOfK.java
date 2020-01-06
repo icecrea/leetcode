b2fb6fcbed96ef55d4f4be8bb37a622c13cb2fd7
icecrea <798167096@qq.com>
2020-01-06 11:51:22 +0800

    '数字在排序数组中出现次数'

--- src/main/java/com/example/swordoffer/Sword37_GetNumberOfK.java
+++ src/main/java/com/example/swordoffer/Sword37_GetNumberOfK.java
@@ -12,45 +12,48 @@ public class Sword37_GetNumberOfK {
         if (length == 0) {
             return 0;
         }
-        int firstK = getFirstK(array, k, 0, length - 1);
-        int lastK = getLastK(array, k, 0, length - 1);
+        int firstK = getFirstK(array, k);
+        int lastK = getLastK(array, k);
         if (firstK != -1 && lastK != -1) {
             return lastK - firstK + 1;
         }
         return 0;
     }
 
-
-    private int getFirstK(int[] array, int k, int start, int end) {
-        if (start > end) {
-            return -1;
+    private int getFirstK(int[] array, int k) {
+        int left = 0, right = array.length - 1, mid;
+        while (left <= right) {
+            mid = (left + right) >>> 1;
+            if (array[mid] == k) {
+                if (mid == 0 || array[mid - 1] != k) {
+                    return mid;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (array[mid] > k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        int mid = start + ((end - start) >> 1);
-        if (array[mid] > k) {
-            return getFirstK(array, k, start, mid - 1);
-        } else if (array[mid] < k) {
-            return getFirstK(array, k, mid + 1, end);
-        } else if (mid - 1 >= 0 && array[mid - 1] == k) {
-            return getFirstK(array, k, start, mid - 1);
-        } else {
-            return mid;
-        }
+        return -1;
     }
 
-    private int getLastK(int[] array, int k, int start, int end) {
-        int length = array.length;
-        int mid = (start + end) >> 1;
-        while (start <= end) {
-            if (array[mid] > k) {
-                end = mid - 1;
-            } else if (array[mid] < k) {
-                start = mid + 1;
-            } else if (mid + 1 < length && array[mid + 1] == k) {
-                start = mid + 1;
+    private int getLastK(int[] array, int k) {
+        int left = 0, right = array.length - 1, mid;
+        while (left <= right) {
+            mid = (left + right) >>> 1;
+            if (array[mid] == k) {
+                if (mid == array.length - 1 || array[mid + 1] != k) {
+                    return mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (array[mid] > k) {
+                right = mid - 1;
             } else {
-                return mid;
+                left = mid + 1;
             }
-            mid = (start + end) >> 1;
         }
         return -1;
     }
