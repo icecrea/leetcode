d4d0cbaac479a23ad91800ef0bd2a441e08a58e1
icecrea <798167096@qq.com>
2020-01-10 00:21:37 +0800

    '二叉树对称-非递归'

--- src/main/java/com/example/topinterview/easy/tree/array49_isSymmetic.java
+++ src/main/java/com/example/topinterview/easy/tree/array49_isSymmetic.java
@@ -2,24 +2,27 @@ package com.example.topinterview.easy.tree;
 
 import com.example.leetcode.linkedlist.pojo.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @description: 对称二叉树
  * 给定一个二叉树，检查它是否是镜像对称的。
  *
  * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
  *
- *     1
- *    / \
- *   2   2
- *  / \ / \
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
  * 3  4 4  3
  * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
  *
- *     1
- *    / \
- *   2   2
- *    \   \
- *    3    3
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
  * 说明:
  *
  * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
@@ -27,24 +30,44 @@ import com.example.leetcode.linkedlist.pojo.TreeNode;
  * @create: 2019-03-18 11:48
  **/
 public class array49_isSymmetic {
-    public boolean isSymmetrical(TreeNode pRoot) {
-        if (pRoot == null) {
-            return true;
-        }
-        return helper(pRoot.left, pRoot.right);
+    public boolean isSymmetricRecur(TreeNode root) {
+        return isMirror(root, root);
     }
 
-    private boolean helper(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) {
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
             return true;
         }
-        if (root1 == null || root2 == null) {
+        if (t1 == null || t2 == null) {
             return false;
         }
-        if (root1.val != root2.val) {
-            return false;
-        }
-        return helper(root1.left, root2.right) && helper(root1.right, root2.left);
-
+        return (t1.val == t2.val)
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
     }
+
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+
 }
