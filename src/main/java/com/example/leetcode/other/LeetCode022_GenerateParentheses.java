86458eb91c675aaebd9b19cf3c63fa764680be96
icecrea <798167096@qq.com>
2020-03-10 21:09:11 +0800

    '回溯法'

---
+++ src/main/java/com/example/leetcode/other/LeetCode022_GenerateParentheses.java
@@ -0,0 +1,51 @@
+package com.example.leetcode.other;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 括号生成
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ * 例如，给出 n = 3，生成结果为：
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @auther: icecrea
+ * @date: 2020/2/21
+ */
+public class LeetCode022_GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, String cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max) {
+            backtrack(ans, cur + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(ans, cur + ")", open, close + 1, max);
+        }
+    }
+
+    @Test
+    public void test() {
+        System.out.println(generateParenthesis(3));
+    }
+
+}
